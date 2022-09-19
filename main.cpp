30c77e206587d5537fe1b6419069acc3776a3f0f
alexxei4 <51131925+alexxei4@users.noreply.github.com>
2022-09-19 17:40:45 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <Arduino.h> 
+ 
+void setup() { 
+  // configure the USB serial monitor 
+  // I , Alexei Ougriniouk , 000776331 , certify this is my work 
+  Serial.begin(115200); 
+  
+} 
+ 
+void loop() { 
+  
+ 
+  // read digitized value from the D1 Mini's A/D convertor 
+  int iVal = analogRead(A0); 
+
+  
+float voltage= iVal * 3.3 / 1024.0;
+  //iVal = map(iVal, 0, 1023, 0, 50);
+
+ 
+  float celsius =  (voltage - 0.33) * 10;
+  
+  
+
+ 
+  // print value to the USB port 
+  Serial.println("Digitized Value = " + String(voltage) + " V"); 
+if(35 < celsius ){
+      Serial.println("Output of " + String(iVal) + " is equal to a temperature input of " + celsius + " C , thats WAY TOO HOT !!"); 
+  }
+    else if(30 <= celsius){
+      Serial.println("Output of " + String(iVal) + " is equal to a temperature input of " + celsius + " C , thats hot!!"); 
+  }
+  else if(25 <= celsius ){
+      Serial.println("Output of " + String(iVal) + " is equal to a temperature input of " + celsius + " C , thats warm!!"); 
+  }
+else if(15 <= celsius){
+      Serial.println("Output of " + String(iVal) + " is equal to a temperature input of " + celsius + " C , thats Perfect!!"); 
+  }
+   else if(10 <= celsius ){
+      Serial.println("Output of " + String(iVal) + " is equal to a temperature input of " + celsius + " C , thats cool!!"); 
+  }
+  else{
+      Serial.println("Output of " + String(iVal) + "is equal to a temperature input of " + celsius + " C , thats cold!!"); 
+  }
+ 
+  // wait 0.5 seconds (500 ms) 
+  delay(500); 
+} 
